eca6da1111efe8a8240d2c9d99ff6b1dda9ec47a
andy-engel <35506553+andy-engel@users.noreply.github.com>
2018-03-09 16:52:47 -0500

    Add files via upload

---
+++ ICE_6/challenge5LAB.js
@@ -0,0 +1,20 @@
+</script>
+function show(var) {
+  document.getElementById('cap').innerHTML = var.alt;
+}
+
+function hide(var) {
+  document.getElementById('cap').innerHTML = '';
+}
+</script>
+
+<p id='cap'></p>
+<img onmouseover='show(this)' onmouseout='hide(this)' src='#' alt='this is the alt text'>
+
+//java functions take quotes in the html
+
+// background-image: url('paper.gif'); css
+
+onfocus
+onblur
+tabindex="1"
